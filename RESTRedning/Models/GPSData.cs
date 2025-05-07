b357b8bb66fabc8458a7cb2feb4826c26aababdd
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-05-07 09:33:41 +0200

    Test is setup for modelclass

--- RESTRedning/Models/GPSData.cs
+++ RESTRedning/Models/GPSData.cs
@@ -1,6 +1,35 @@
-﻿namespace RESTRedning.Models
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RESTRedning.Models
 {
     public class GPSData
     {
+        private int _id;
+        private int _year;
+        private int _month;
+        private int _day;
+        private int _hour;
+        private int _minute;
+        private double _latitude;
+        private double _longitude;
+        private double _speedKnots;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                 _id = value;
+
+            }
+
+        }
+
+
+        public override string ToString() {
+            return "";
+        }
+
+
     }
 }
---
+++ RESTRedningTests/Models/GPSDataTests.cs
@@ -0,0 +1,18 @@
+﻿using RESTRedning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTRedning.Models.Tests
+{
+    [TestClass()]
+    public class GPSDataTests
+    {
+        [TestMethod()]
+        public void ToStringTest() {
+            Assert.Fail();
+        }
+    }
+}
