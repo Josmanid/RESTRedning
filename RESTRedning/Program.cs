8ff8b8c77303bb7fbf074400e29432a270153c75
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-05-08 14:25:39 +0200

    Added cors

--- RESTRedning/Program.cs
+++ RESTRedning/Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                              });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSingleton<GPSDataRep>(new GPSDataRep());
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +28,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
