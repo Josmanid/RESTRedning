5f4c573b2ee555111355645b942c030b1b8641f1
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-05-15 13:31:23 +0200

    Repository template made

--- RESTRedning/Repositories/GPSDataDbRep.cs
+++ RESTRedning/Repositories/GPSDataDbRep.cs
@@ -1,36 +1,38 @@
 ﻿using RESTRedning.DBContext;
 using RESTRedning.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RESTRedning.Repositories
 {
-    public class GPSDataDbRep : GPSDataDbContext
+    public class GPSDataDbRep 
     {
 
         private readonly GPSDataDbContext _dbContext;
-        public List<GPSData>? GPSDataList { get; private set; }
-        public GPSDataDbRep(GPSDataDbContext dbContext, List<GPSData>? gPSDataList)
+        public GPSDataDbRep(GPSDataDbContext dbContext)
         {
             _dbContext = dbContext;
-            GPSDataList = gPSDataList;
+           
         }
 
-        public GPSDataDbRep() {
-            GPSDataList = new List<GPSData>();
-        }
 
         public IEnumerable<GPSData> GetGPSData() {
-            IEnumerable<GPSData> result = new List<GPSData>(GPSDataList);
+            IEnumerable<GPSData> result = _dbContext.GPSDataB.ToList().AsQueryable();
 
             return result;
         }
 
 
         public GPSData AddGPSData(GPSData GPSData) {
+            GPSData.Id = 0;
             if (GPSData == null)
             {
                 throw new ArgumentNullException("GPS data cannot be null");
             }
-            GPSDataList.Add(GPSData);
+            _dbContext.GPSDataB.Add(GPSData);
+            _dbContext.SaveChanges();
             return GPSData;
         }
 
