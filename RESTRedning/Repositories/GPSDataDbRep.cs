b4c5c3c831e6158189b494058dece760d947e778
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-05-15 13:06:57 +0200

    DB Unit template made

---
+++ RESTRedning/Repositories/GPSDataDbRep.cs
@@ -0,0 +1,33 @@
+﻿using RESTRedning.Models;
+
+namespace RESTRedning.Repositories
+{
+    public class GPSDataDbRep
+    {
+        public List<GPSData>? GPSDataList { get; private set; }
+
+
+        public GPSDataDbRep() {
+            GPSDataList = new List<GPSData>();
+        }
+
+        public IEnumerable<GPSData> GetGPSData() {
+            IEnumerable<GPSData> result = new List<GPSData>(GPSDataList);
+
+            return result;
+        }
+
+
+        public GPSData AddGPSData(GPSData GPSData) {
+            if (GPSData == null)
+            {
+                throw new ArgumentNullException("GPS data cannot be null");
+            }
+            GPSDataList.Add(GPSData);
+            return GPSData;
+        }
+
+
+
+    }
+}
---
+++ RESTRedningTests/Repositories/GPSDataDbRepTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTRedning.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTRedning.Repositories.Tests
+{
+    [TestClass()]
+    public class GPSDataDbRepTests
+    {
+        [TestMethod()]
+        public void GPSDataDbRepTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGPSDataTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddGPSDataTest() {
+            Assert.Fail();
+        }
+    }
+}
